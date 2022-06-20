ae446265097b5caef1821fadecf6273014ddbf26
Niall <NiallHarrington12@gmail.com>
2022-06-20 12:50:11 +0700

    Added some leap year test function

--- LeapYearTester.py
+++ LeapYearTester.py
@@ -1,3 +1,36 @@
+def leap_year(year):
+    if year % 100 == 0 and year % 400 != 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+month_length = {"January": 0,
+                "February": 0,
+                "March": 0,
+                "April": 0,
+                "May": 0,
+                "June": 0,
+                "July": 0,
+                "August": 0,
+                "September": 0,
+                "October": 0,
+                "November": 0,
+                "December": 0}
+sentence = "30 days in September, April, June and November"
+for i in month_length:
+    if i in sentence:
+        month_length[i] = 30
+    elif i == "February":
+        month_length[i] = 28
+    elif i not in sentence:
+        month_length[i] = 31
+
+print(month_length)
+
+
 month_length = {"January": 0,
                 "February": 0,
                 "March": 0,
