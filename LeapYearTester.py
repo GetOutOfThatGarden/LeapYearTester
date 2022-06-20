232463acf1249c28601e910091367493fef19994
Niall <NiallHarrington12@gmail.com>
2022-06-20 12:58:30 +0700

    Updated file, I had an issue with saving the file, so I had to overwrite the last one.

--- LeapYearTester.py
+++ LeapYearTester.py
@@ -21,37 +21,13 @@ month_length = {"January": 0,
                 "December": 0}
 sentence = "30 days in September, April, June and November"
 
-year = 2020
+year = 1900
 
 for i in month_length:
     if i in sentence:
         month_length[i] = 30
     elif i == "February" and leap_year(year) == True:
         month_length[i] = 29
-    elif i == "February":
-        month_length[i] = 28
-    elif i not in sentence:
-        month_length[i] = 31
-
-print(month_length)
-
-
-month_length = {"January": 0,
-                "February": 0,
-                "March": 0,
-                "April": 0,
-                "May": 0,
-                "June": 0,
-                "July": 0,
-                "August": 0,
-                "September": 0,
-                "October": 0,
-                "November": 0,
-                "December": 0}
-sentence = "30 days in September, April, June and November"
-for i in month_length:
-    if i in sentence:
-        month_length[i] = 30
     elif i == "February":
         month_length[i] = 28
     elif i not in sentence:
