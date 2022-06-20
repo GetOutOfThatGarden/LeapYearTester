d305c152ea4882d8dfd7f9d082f8c693d17dec21
Niall <NiallHarrington12@gmail.com>
2022-06-20 12:55:42 +0700

    Changed the for look to check for a leap year.

--- LeapYearTester.py
+++ LeapYearTester.py
@@ -20,9 +20,14 @@ month_length = {"January": 0,
                 "November": 0,
                 "December": 0}
 sentence = "30 days in September, April, June and November"
+
+year = 2020
+
 for i in month_length:
     if i in sentence:
         month_length[i] = 30
+    elif i == "February" and leap_year(year) == True:
+        month_length[i] = 29
     elif i == "February":
         month_length[i] = 28
     elif i not in sentence:
